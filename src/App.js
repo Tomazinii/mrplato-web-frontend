f009ae8dc2f6cef81cb15ef3e4ca8f396d50090a
alotropo <jhontomazini@hotmail.com>
2023-10-10 15:58:28 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn dasasddsasd sad dsaasddas sda asd sda sdadsqwewqewqe
         </a>
       </header>
     </div>
